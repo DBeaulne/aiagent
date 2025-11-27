38bfc625a098f96fa9f5c83fa255c7d3b0cf7802
DBeaulne <daniel.beaulne.dev@gmail.com>
2025-11-27 16:41:50 -0500

    begin integrating logic to get user input for agent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,8 +9,14 @@ client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from aiagent!")
+    if len(sys.argv) < 2:
+        print("Please provide a prompt for the agent")
+        sys.exit(1)
+    
+    agent_model = 'gemini-2.0-flash-001'
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model=agent_model, contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
