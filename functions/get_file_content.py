16cfe908af5d1ef8d01e8233a265258643ff88cb
DBeaulne <daniel.beaulne.dev@gmail.com>
2025-12-02 16:41:45 -0500

    add get_file_info logic and testing

---
+++ functions/get_file_content.py
@@ -0,0 +1,9 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_file.startswith(abs_working_dir)
+
+    pass
