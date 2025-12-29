3a8a99f824c42139803475f382630fd1d2ce4170
DBeaulne <daniel.beaulne.dev@gmail.com>
2025-12-29 16:03:05 -0500

    created stand-alon generate_content.py file in functions folder

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.generate_content import generate_content
 
-from call_function import available_functions, call_function
-from prompts import system_prompt
 
 
 def main():
@@ -26,41 +25,6 @@ def main():
         print(f"User prompt: {args.user_prompt}\n")
 
     generate_content(client, messages, args.verbose)
-
-
-def generate_content(client, messages, verbose):
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
-    if not response.usage_metadata:
-        raise RuntimeError("Gemini API response appears to be malformed")
-
-    if verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-
-    if not response.function_calls:
-        print("Response:")
-        print(response.text)
-        return
-
-    function_responses = []
-    for function_call in response.function_calls:
-        result = call_function(function_call, verbose)
-        if (
-            not result.parts
-            or not result.parts[0].function_response
-            or not result.parts[0].function_response.response
-        ):
-            raise RuntimeError(f"Empty function response for {function_call.name}")
-        if verbose:
-            print(f"-> {result.parts[0].function_response.response}")
-        function_responses.append(result.parts[0])
-
 
 if __name__ == "__main__":
     main()
